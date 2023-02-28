6af3e310039b82e8c902cbb333d2aa37b1d379e1
Vlad Zhuravlev <vzhuravlev.laba@solvd.com>
2023-02-28 14:00:40 +0300

    checking

---
+++ src/test/java/com/qaprosoft/carina/demo/MySecondWebTest.java
@@ -0,0 +1,18 @@
+package com.qaprosoft.carina.demo;
+
+import com.zebrunner.carina.utils.R;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import java.lang.invoke.MethodHandles;
+
+public class MySecondWebTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Test
+    public void test1(){
+
+    }
+}
